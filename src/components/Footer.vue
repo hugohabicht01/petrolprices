f2873641d82ca20b240f181ebe531ec1b5566a30
hugohabicht01 <anon@example.org>
2022-08-04 18:50:32 +0200

    add settings page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -24,11 +24,18 @@ const toggleLocales = () => {
       <div i-carbon-language />
     </a>
 
+    <RouterLink class="icon-btn mx-2" :title="t('button.settings')" to="/settings">
+      <div i-carbon-settings />
+    </RouterLink>
+
     <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
       <div i-carbon-dicom-overlay />
     </RouterLink>
 
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a
+      class="icon-btn mx-2" rel="noreferrer" href="https://github.com/hugohabicht01/petrolprices" target="_blank"
+      title="GitHub"
+    >
       <div i-carbon-logo-github />
     </a>
   </nav>
---
+++ src/pages/settings.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useStationsStore } from '~/stores/stations'
+import { useLocationStore } from '~/stores/location'
+
+const stations = useStationsStore()
+const { reset: resetLocation } = useLocationStore()
+
+// const { doFetch, forceRefreshPrices } = stations
+
+const { radius } = storeToRefs(stations)
+</script>
+
+<template>
+  <div grid grid-cols-2 grid-flow-row justify-items-start auto-rows-auto p-2 w-max grid-gap-4>
+    <h1 font-bold text-8 col-span-2>
+      Settings
+    </h1>
+    <div mt-2 contents>
+      <label for="resetGPSbtn">Reset GPS tracking</label>
+      <input
+        id="resetGPSbtn" type="button" dark:bg-gray-200 dark:text-gray-700 bg-gray-300 py-2 px-4 rounded
+        value="Reset" @click="resetLocation"
+      >
+      <label for="radiusrange">Change search radius</label>
+      <input id="radiusrange" v-model="radius" type="range" min="1" max="20" step="0.5">
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
